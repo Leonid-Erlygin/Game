dbef3a6299b7058b6dbdd752f76cecf1b02ccdb8
Leonid-Erlygin <erlygin.la@phystech.edu>
2020-12-19 15:13:23 +0300

    fix7

--- cmake-build-debug/CMakeFiles/Box2D.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Box2D.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/Box2D.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Box2D.
-# This may be replaced when dependencies are built.
